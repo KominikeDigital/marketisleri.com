f086c2ba01d3d01bc85932b8c723cfa5113b0f72
emre <info@kominikee.com>
2026-06-08 14:19:32 +0300

    çööç

--- scraper/generic.js
+++ scraper/generic.js
@@ -46,10 +46,14 @@ function downloadFile(url, destPath) {
             fs.mkdirSync(dir, { recursive: true });
         }
         
+        const parsedUrl = new URL(url);
+        const origin = `${parsedUrl.protocol}//${parsedUrl.host}/`;
+        
         const file = fs.createWriteStream(destPath);
         https.get(url, {
             headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+                'Referer': origin
             }
         }, (response) => {
             if (response.statusCode !== 200) {
