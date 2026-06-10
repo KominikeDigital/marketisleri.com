cee70c96a7cd96ea4efe4283e46c5671896b7b11
emre <info@kominikee.com>
2026-06-10 18:17:19 +0300

    debug: enable error reporting in analyze_brochures.php

--- admin/analyze_brochures.php
+++ admin/analyze_brochures.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 /**
  * admin/analyze_brochures.php
  * Broşür sayfalarını Gemini Vision API ile analiz etmek için admin paneli.
